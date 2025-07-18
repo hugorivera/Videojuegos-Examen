32146faeab70df1a8c4f0ac6c813ce2587e9deb4
Hugo Rivera <riveraquevedoh@gmail.com>
2025-07-18 11:39:30 -0600

    Initial Commit

---
+++ Videojuegos/VideojuegosApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideojuegosApp.swift
+//  Videojuegos
+//
+//  Created by Hugo Rivera on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct VideojuegosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
